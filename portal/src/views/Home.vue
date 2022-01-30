fd212019ee61c783bf1ee7c867600e1da9a199ec
George Ioannou <gmioannou@gmail.com>
2022-01-30 18:42:58 +0200

    rename project

---
+++ portal/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="p-4">Town Planning - Medscape {{ geoserverUrl }}</div>
+    <map-view></map-view>
+  </div>
+</template>
+
+<script>
+import MapView from '@/views/MapView'
+export default {
+  name: 'Home',
+  components: {
+    MapView
+  },
+  data() {
+    return {
+      geoserverUrl: ''
+    }
+  },
+  mounted() {
+    this.geoserverUrl = process.env.VUE_APP_GEOSERVER_URL
+  }
+}
+</script>
